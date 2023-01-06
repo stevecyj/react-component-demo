1e48adc0857abc36e3b5f66846fab258899b085e
Steve <ycsteven.tsao@gmail.com>
2023-01-07 07:05:11 +0800

    comment crud

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import './index.css'
-import avatar from './images/avatar.png'
-import React from 'react'
+import './index.css';
+import avatar from './images/avatar.png';
+import React from 'react';
+import { v4 as uuid } from 'uuid';
 
 // 时间格式化
-function formatDate (time) {
-  return `${time.getFullYear()}-${time.getMonth()}-${time.getDate()}`
+function formatDate(time) {
+  return `${time.getFullYear()}-${time.getMonth()}-${time.getDate()}`;
 }
 
 class App extends React.Component {
@@ -48,12 +49,57 @@ class App extends React.Component {
         // 1: 点赞 0：无态度 -1:踩
         attitude: -1
       }
-    ]
-  }
-  switchTab = (type) => {
-    console.log('switch',type);
+    ],
+    comment: '請輸入內容'
   };
-  render () {
+  switchTab = (type) => {
+    console.log('switch', type);
+    this.setState({ active: type });
+  };
+
+  textareaChange = (e) => {
+    this.setState({ comment: e.target.value });
+  };
+
+  submitComment = () => {
+    this.setState({
+      list: [
+        ...this.state.list,
+        {
+          id: uuid(),
+          author: 'JOJO',
+          comment: this.state.comment,
+          time: new Date(),
+          // 1: 点赞 0：无态度 -1:踩
+          attitude: 0
+        }
+      ]
+    });
+  };
+  delComment = (id) => {
+    console.log(id);
+    this.setState({
+      list: this.state.list.filter(item => item.id !== id)
+    });
+  };
+  toggleLike = (currItem) => {
+    console.log(currItem);
+    const { attitude, id } = currItem;
+    this.setState({
+      list: this.state.list.map(item => {
+        if (item.id === id) {
+          return {
+            ...item,
+            attitude: attitude === 1 ? 0 : 1
+          };
+        } else {
+          return item;
+        }
+      })
+    });
+  };
+
+  render() {
     return (
       <div className="App">
         <div className="comment-container">
@@ -68,7 +114,7 @@ class App extends React.Component {
                 this.state.tabs.map(tab => {
                   return (
                     <li
-                      onClick={()=>this.switchTab(tab.type)}
+                      onClick={() => this.switchTab(tab.type)}
                       key={tab.id}
                       className={tab.type === this.state.active ? 'on' : ''}
                     >按{tab.name}排序</li>
@@ -81,7 +127,7 @@ class App extends React.Component {
           {/* 添加评论 */}
           <div className="comment-send">
             <div className="user-face">
-              <img className="user-head" src={avatar} alt="" />
+              <img className="user-head" src={avatar} alt=""/>
             </div>
             <div className="textarea-container">
               <textarea
@@ -89,8 +135,10 @@ class App extends React.Component {
                 rows="5"
                 placeholder="发条友善的评论"
                 className="ipt-txt"
+                value={this.state.comment}
+                onChange={this.textareaChange}
               />
-              <button className="comment-submit">发表评论</button>
+              <button className="comment-submit" onClick={this.submitComment}>发表评论</button>
             </div>
             <div className="comment-emoji">
               <i className="face"></i>
@@ -104,20 +152,21 @@ class App extends React.Component {
               this.state.list.map(item => (
                 <div className="list-item" key={item.id}>
                   <div className="user-face">
-                    <img className="user-head" src={avatar} alt="" />
+                    <img className="user-head" src={avatar} alt=""/>
                   </div>
                   <div className="comment">
                     <div className="user">{item.author}</div>
                     <p className="text">{item.comment}</p>
                     <div className="info">
                       <span className="time">{formatDate(item.time)}</span>
-                      <span className={item.attitude === 1 ? 'like liked' : 'like'}>
-                        <i className="icon" />
+                      <span className={item.attitude === 1 ? 'like liked' : 'like'}
+                            onClick={() => this.toggleLike(item)}>
+                        <i className="icon"/>
                       </span>
                       <span className={item.attitude === -1 ? 'hate hated' : 'hate'}>
-                        <i className="icon" />
+                        <i className="icon"/>
                       </span>
-                      <span className="reply btn-hover">删除</span>
+                      <span className="reply btn-hover" onClick={() => this.delComment(item.id)}>删除</span>
                     </div>
                   </div>
                 </div>
@@ -125,9 +174,8 @@ class App extends React.Component {
             }
           </div>
         </div>
-      </div>)
+      </div>);
   }
 }
 
-
-export default App
+export default App;
